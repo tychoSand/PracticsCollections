eaa8af37ea7710ae685fa7350b526748a2b79504
tychoSand <157190816+tychoSand@users.noreply.github.com>
2024-01-19 20:39:16 -0800

    libreria erronea modificada

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.io.*;
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println(getTextTest());
