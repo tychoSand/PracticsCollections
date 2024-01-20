360fe70b1448e83d681372d232f52ed2158bbe38
manue <dam2600@gmail.com>
2024-01-19 20:27:38 -0800

    metodo de suma con dos dígitos

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,29 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println(getTextTest());
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Dame tu primer numero: ");
+        int x = scan.nextInt();
+        System.out.println("Dame tu segundo numero: ");
+        int y = scan.nextInt();
+
+        System.out.println("la suma es: "+sumaNumeros(x,y));
+
     }
 
         public static String getTextTest () {
         return "extrac text " ;
+        }
+        public static int sumaNumeros (int x, int y ){
+
+            int result = 0;
+
+            result = x + y;
+
+            return result;
         }
     }
 
